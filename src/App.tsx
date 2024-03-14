e99f92911184f99c5a2054203da2f5e5ec124bcf
PidzhakovIlya <pidzhakovilja@gmail.com>
2024-03-14 20:11:46 +0300

    add delete the task

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 import {Todolist} from "./Todolist";
 
 const date1 = new Date(2024, 3, 14, 14, 5)
 const date2 = new Date(2024, 3, 14, 15, 5)
 
 export type InitialDataType = Array<TaskType>
-export type InitialStateType = { data:Array<TaskType> }
+export type InitialStateType = { data: Array<TaskType> }
 
 type TaskType = {
     title: string
@@ -18,16 +18,16 @@ type TaskType = {
 
 const initialData: InitialDataType = [
     {
-        title: 'Learn React',
-        desc: 'Побыстрее',
+        title: "Learn React",
+        desc: "Побыстрее",
         image: "",
         done: true,
         createdAt: date1.toLocaleDateString(),
         key: date1.getTime()
     },
     {
-        title: 'Изучить классы',
-        desc: 'Побыстрее',
+        title: "Изучить классы",
+        desc: "Побыстрее",
         image: "",
         done: false,
         createdAt: date2.toLocaleDateString(),
@@ -37,17 +37,23 @@ const initialData: InitialDataType = [
 
 class App extends React.Component<any, InitialStateType> {
     state: InitialStateType
+
     constructor(props: any) {
         super(props);
-        this.state = {data:initialData}
+        this.state = {data: initialData}
         this.setDown = this.setDown.bind(this)
+        this.delete = this.delete.bind(this)
     }
 
+    delete(key: number) {
+        const newData = this.state.data.filter(t => t.key !== key)
+        this.setState({data: newData})
+    }
 
     setDown(key: number) {
-        const deed:TaskType = this.state.data.find((t: any) => t.key === key) as TaskType
+        const deed: TaskType = this.state.data.find((t: any) => t.key === key) as TaskType
         if (deed) deed.done = true;
-        this.setState((state:InitialStateType)=>({}))
+        this.setState((state: InitialStateType) => ({}))
     }
 
     render() {
@@ -55,14 +61,15 @@ class App extends React.Component<any, InitialStateType> {
             <div>
                 <nav className="navbar is-light ">
                     <div className="navbar-brand">
-        <span className={'navbar-item is-uppercase'}>
+        <span className={"navbar-item is-uppercase"}>
             Todos
         </span>
                     </div>
                 </nav>
                 <main className="content px-6 mt-6">
                     <Todolist list={this.state.data}
-                              setDown={this.setDown}/>
+                              setDown={this.setDown}
+                              delete={this.delete}/>
                 </main>
             </div>
         );
--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,29 +1,34 @@
-import React from 'react';
+import React from "react";
 import {InitialDataType} from "./App";
 
 
 type TodoPropsTyp = {
     list: InitialDataType
-    setDown:(key:number)=>void
+    setDown: (key: number) => void
+    delete: (key: number) => void
 }
 
-export function Todolist(props:TodoPropsTyp) {
+export function Todolist(props: TodoPropsTyp) {
     return (
         <section>
             <h1>Задачи</h1>
-            <table className={'table is-hoverable if-fullwidth'}>
+            <table className={"table is-hoverable if-fullwidth"}>
                 <tbody>
-                {props.list.map(t=>(
+                {props.list.map(t => (
                     <tr key={t.key}>
                         <td>
                             {t.done && <del>{t.title}</del>}
                             {!t.done && t.title}
                         </td>
                         <td>
-                            <button className='button is-success' title='Поместить как сделанное' disabled={t.done} onClick={()=>props.setDown(t.key)}>&#9745;</button>
+                            <button className="button is-success"
+                                    title="Поместить как сделанное"
+                                    disabled={t.done}
+                                    onClick={() => props.setDown(t.key)}>&#9745;</button>
                         </td>
                         <td>
-                            <button className='button is-danger' title='Удалить'>&#9746;</button>
+                            <button className="button is-danger" title="Удалить"
+                                    onClick={() => props.delete(t.key)}>&#9746;</button>
                         </td>
                     </tr>
                 ))}
