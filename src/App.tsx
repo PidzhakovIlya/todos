d5961d51411769725aacab328fa5b445d232941e
PidzhakovIlya <pidzhakovilja@gmail.com>
2024-03-14 16:27:18 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,72 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Todolist} from "./Todolist";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const date1 = new Date(2024, 3, 14, 14, 5)
+const date2 = new Date(2024, 3, 14, 15, 5)
+
+export type InitialDataType = Array<TaskType>
+export type InitialStateType = { data:Array<TaskType> }
+
+type TaskType = {
+    title: string
+    desc: string
+    image: string
+    done: boolean
+    createdAt: string
+    key: number
+}
+
+const initialData: InitialDataType = [
+    {
+        title: 'Learn React',
+        desc: 'Побыстрее',
+        image: "",
+        done: true,
+        createdAt: date1.toLocaleDateString(),
+        key: date1.getTime()
+    },
+    {
+        title: 'Изучить классы',
+        desc: 'Побыстрее',
+        image: "",
+        done: false,
+        createdAt: date2.toLocaleDateString(),
+        key: date2.getTime()
+    }
+]
+
+class App extends React.Component<any, InitialStateType> {
+    state: InitialStateType
+    constructor(props: any) {
+        super(props);
+        this.state = {data:initialData}
+        this.setDown = this.setDown.bind(this)
+    }
+
+
+    setDown(key: number) {
+        const deed:TaskType = this.state.data.find((t: any) => t.key === key) as TaskType
+        if (deed) deed.done = true;
+        this.setState((state:InitialStateType)=>({}))
+    }
+
+    render() {
+        return (
+            <div>
+                <nav className="navbar is-light ">
+                    <div className="navbar-brand">
+        <span className={'navbar-item is-uppercase'}>
+            Todos
+        </span>
+                    </div>
+                </nav>
+                <main className="content px-6 mt-6">
+                    <Todolist list={this.state.data}
+                              setDown={this.setDown}/>
+                </main>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/Todolist.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import {InitialDataType} from "./App";
+
+
+type TodoPropsTyp = {
+    list: InitialDataType
+    setDown:(key:number)=>void
+}
+
+export function Todolist(props:TodoPropsTyp) {
+    return (
+        <section>
+            <h1>Задачи</h1>
+            <table className={'table is-hoverable if-fullwidth'}>
+                <tbody>
+                {props.list.map(t=>(
+                    <tr key={t.key}>
+                        <td>
+                            {t.done && <del>{t.title}</del>}
+                            {!t.done && t.title}
+                        </td>
+                        <td>
+                            <button className='button is-success' title='Поместить как сделанное' disabled={t.done} onClick={()=>props.setDown(t.key)}>&#9745;</button>
+                        </td>
+                        <td>
+                            <button className='button is-danger' title='Удалить'>&#9746;</button>
+                        </td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </section>
+    );
+}
+
+
+
